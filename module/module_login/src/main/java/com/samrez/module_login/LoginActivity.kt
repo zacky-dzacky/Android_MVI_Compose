12e0c4cf5b0359fa78e55adb96bf6e2e02326522
zackysyarief <zackydzacky@gmail.com>
2024-02-16 20:07:48 +0700

    [Compose] Column and Row in compose

--- module/module_login/src/main/java/com/samrez/module_login/LoginActivity.kt
+++ module/module_login/src/main/java/com/samrez/module_login/LoginActivity.kt
@@ -1,16 +1,31 @@
 package com.samrez.module_login
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.samrez.module_login.ui.theme.AndroidMVIComposeTheme
+import id.syarief.android_mvi_compose.module_login.R
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +52,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    name = "Light Mode"
+)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode")
 @Composable
 fun GreetingPreview() {
     AndroidMVIComposeTheme {
-        Greeting("Android")
+        Row (modifier = Modifier.padding(all = 16.dp)) {
+            Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape))
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                for(i in 0..3){
+                    Greeting(name = "Android")
+                    Spacer(modifier = Modifier.height(4.dp))
+                }
+            }
+        }
+
     }
 }
