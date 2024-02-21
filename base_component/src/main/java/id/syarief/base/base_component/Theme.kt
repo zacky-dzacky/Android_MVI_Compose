cfa85e91f7fd4185764c03ae5c2c68304fd27469
zackysyarief <zackydzacky@gmail.com>
2024-02-21 20:24:32 +0700

    [Compose] update splashscreen to with android 12 behaviour

--- base_component/src/main/java/id/syarief/base/base_component/Theme.kt
+++ base_component/src/main/java/id/syarief/base/base_component/Theme.kt
@@ -1,11 +1,20 @@
 package id.syarief.base.base_component
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
 private val DarkColorPalette = darkColorScheme(
     primary = Color.Blue,
@@ -30,15 +39,30 @@ private val LightColorPalette = lightColorScheme(
     */
 )
 @Composable
-fun BaseTheme(darkTheme: Boolean = isSystemInDarkTheme(),  content: @Composable () -> Unit) {
+fun BaseTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
 
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
     }
     MaterialTheme(
-        colorScheme = colors,
-        content = content
+        colorScheme = colorScheme,
+        content = content,
+        typography = Typography
     )
 }
--- module/module_home/src/main/java/id/syarief/android_mvi_compose/module_home/WaterCounter.kt
+++ module/module_home/src/main/java/id/syarief/android_mvi_compose/module_home/WaterCounter.kt
@@ -1,9 +1,17 @@
 package id.syarief.android_mvi_compose.module_home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -11,9 +19,30 @@ import id.syarief.base.base_component.BaseTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier){
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    var count by rememberSaveable { mutableStateOf(0) }
+    Column {
+        if (count > 0) {
+            Text(
+                text = "You've had $count glasses.",
+                modifier = modifier.padding(16.dp)
+            )
+        }
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 10.dp),
+            enabled = count < 10
+        ) {
+            Text(text = "Add One")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Preview(){
+    BaseTheme {
+        Surface(Modifier.fillMaxSize()) {
+            WaterCounter()
+        }
+    }
 }
